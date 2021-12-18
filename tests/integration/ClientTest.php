ad607a1e77722b6476cef114d10947015355d78d
Jacques Marneweck <jacques@siberia.co.za>
2021-12-18 19:16:42 +0000

    Update tests for newer phpunit version

--- tests/integration/ClientTest.php
+++ tests/integration/ClientTest.php
@@ -3,19 +3,21 @@
  * Client for Freepaid's SOAP interface.
  *
  * @author    Jacques Marneweck <jacques@powertrip.co.za>
- * @copyright 2013-2019 Jacques Marneweck.  All rights strictly reserved.
+ * @copyright 2013-2021 Jacques Marneweck.  All rights strictly reserved.
  * @license   MIT
  */
 
 namespace Jacques\Freepaid\Tests\Integration;
 
-class ClientTest extends \PHPUnit\Framework\TestCase
+use Brick\VarExporter\VarExporter;
+
+final class ClientTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
     }
 
@@ -23,64 +25,64 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
     }
 
     /**
      * @covers \Jacques\Freepaid\Client::__construct
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Please provide a url to use to connect to Freepaid.
      */
-    public function testEmptyContructor()
+    public function testEmptyContructor(): void
     {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Please provide a url to use to connect to Freepaid.');
         $client = new \Jacques\Freepaid\Client();
         $this->assertNotNull($client);
-        $this->assertInstanceOf('\Jacques\Freepaid\Client', $client);
+        $this->assertInstanceOf(\Jacques\Freepaid\Client::class, $client);
     }
 
     /**
      * @covers \Jacques\Freepaid\Client::__construct
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Please provide a url to use to connect to Freepaid.
      */
-    public function testContructorOptionsMissingHostname()
+    public function testContructorOptionsMissingHostname(): void
     {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Please provide a url to use to connect to Freepaid.');
         $client = new \Jacques\Freepaid\Client([
             'username' => '12345678',
             'password' => 'password',
         ]);
         $this->assertNotNull($client);
-        $this->assertInstanceOf('\Jacques\Freepaid\Client', $client);
+        $this->assertInstanceOf(\Jacques\Freepaid\Client::class, $client);
     }
 
     /**
      * @covers \Jacques\Freepaid\Client::__construct
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Please provide the username to use for requests to Freepaid.
      */
-    public function testContructorOptionsMissingUsername()
+    public function testContructorOptionsMissingUsername(): void
     {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Please provide the username to use for requests to Freepaid.');
         $client = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
         ]);
         $this->assertNotNull($client);
-        $this->assertInstanceOf('\Jacques\Freepaid\Client', $client);
+        $this->assertInstanceOf(\Jacques\Freepaid\Client::class, $client);
     }
 
     /**
      * @covers \Jacques\Freepaid\Client::__construct
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Please provide the password to use for requests to Freepaid.
      */
-    public function testContructorOptionsMissingPassword()
+    public function testContructorOptionsMissingPassword(): void
     {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Please provide the password to use for requests to Freepaid.');
         $client = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
             'username' => '12345678',
         ]);
         $this->assertNotNull($client);
-        $this->assertInstanceOf('\Jacques\Freepaid\Client', $client);
+        $this->assertInstanceOf(\Jacques\Freepaid\Client::class, $client);
     }
 
     /**
@@ -88,7 +90,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::getBalance
      * @vcr unittest_integration_getbalance
      */
-    public function testGetBalance()
+    public function testGetBalance(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -97,7 +99,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
         ]);
         $response = $freepaid->getBalance();
 
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(1, $response->status);
         $this->assertEquals('000', $response->errorcode);
         $this->assertEquals('-', $response->message);
@@ -109,7 +111,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::getLastTransaction
      * @vcr unittest_integration_getlasttansaction
      */
-    public function testGetLastTransaction()
+    public function testGetLastTransaction(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -118,13 +120,13 @@ class ClientTest extends \PHPUnit\Framework\TestCase
         ]);
         $response = $freepaid->getLastTransaction('2016120316274783');
 
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(1, $response->status);
         $this->assertEquals('100', $response->errorcode);
         $this->assertEquals('-', $response->message);
         $this->assertEquals('0', $response->balance);
         $this->assertEquals('-', $response->orderno);
-        $this->assertInternalType('array', $response->vouchers);
+        $this->assertIsArray($response->vouchers);
         $this->assertCount(0, $response->vouchers);
     }
 
@@ -133,7 +135,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::pinbased
      * @vcr unittest_integration_pinbased_single_vodacom_r2
      */
-    public function testPinbasedSingleVodacomR2()
+    public function testPinbasedSingleVodacomR2(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -144,7 +146,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
 
         $response = $freepaid->pinbased('vodacom', '2', $uuid);
 
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(1, $response->status);
         $this->assertEquals('000', $response->errorcode);
         $this->assertEquals('success', $response->message);
@@ -157,7 +159,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::getTransaction
      * @vcr unittest_integration_gettransaction_single_vodacom_r2
      */
-    public function testGetTransactionSingleVodacomR2()
+    public function testGetTransactionSingleVodacomR2(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -167,32 +169,25 @@ class ClientTest extends \PHPUnit\Framework\TestCase
 
         $response = $freepaid->getTransaction('2016120316274783');
 
-        $this->assertInstanceOf('\stdClass', $response);
-        $this->assertEquals(1, $response->status);
-        $this->assertEquals('000', $response->errorcode);
-        $this->assertEquals('-', $response->message);
-        $this->assertEquals('99980.09', $response->balance);
-        $this->assertEquals('2016120316274783', $response->orderno);
-        $this->assertInternalType('array', $response->vouchers);
-        $this->assertCount(1, $response->vouchers);
+	//echo VarExporter::export($response);
 
-        for ($i = 0; $i < 1; $i++) {
-            $this->assertInstanceOf('\stdClass', $response->vouchers[$i]);
-            $this->assertInternalType('string', $response->vouchers[$i]->network);
-            $this->assertInternalType('double', $response->vouchers[$i]->sellvalue);
-            $this->assertInternalType('string', $response->vouchers[$i]->pin);
-            $this->assertInternalType('string', $response->vouchers[$i]->serial);
-            $this->assertInternalType('double', $response->vouchers[$i]->costprice);
-        }
-
-        /*
-         * 0
-         */
-        $this->assertEquals('vodacom', $response->vouchers['0']->network);
-        $this->assertEquals(2, $response->vouchers['0']->sellvalue);
-        $this->assertEquals('125073790182', $response->vouchers['0']->pin);
-        $this->assertEquals('524516544', $response->vouchers['0']->serial);
-        $this->assertEquals(1.81, $response->vouchers['0']->costprice);
+	$expected = (object) [
+	    'status' => 1,
+	    'errorcode' => '000',
+	    'message' => '-',
+	    'balance' => 99980.09,
+	    'orderno' => '2016120316274783',
+	    'vouchers' => [
+		(object) [
+		    'network' => 'vodacom',
+		    'sellvalue' => 2.0,
+		    'pin' => '125073790182',
+		    'serial' => '524516544',
+		    'costprice' => 1.81
+		]
+	    ]
+	];
+	self::assertEquals($expected, $response);
     }
 
     /**
@@ -200,7 +195,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::getTransactionStatus
      * @vcr unittest_integration_gettransactionstatus_single_vodacom_r2
      */
-    public function testGetTransactionStatusSingleVodacomR2()
+    public function testGetTransactionStatusSingleVodacomR2(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -210,7 +205,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
 
         $response = $freepaid->getTransactionStatus('2016120316274783');
 
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(0, $response->status);
         $this->assertEquals('112', $response->errorcode);
         $this->assertEquals('orderno [2016120316274783] not found?', $response->message);
@@ -224,7 +219,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::pinless
      * @vcr unittest_integration_pinless_single_vodacom_r2
      */
-    public function testPinlessSingleVodacomR2()
+    public function testPinlessSingleVodacomR2(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -234,7 +229,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
         $uuid = 'd63f2ecc-b96b-11e6-8ef6-28cfe91331d9';
 
         $response = $freepaid->pinless('vodacom', '27811234567', '2', $uuid);
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(1, $response->status);
         $this->assertEquals('000', $response->errorcode);
         $this->assertEquals('success', $response->message);
@@ -247,7 +242,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
      * @covers \Jacques\Freepaid\Client::getProducts
      * @vcr unittest_integration_getproducts
      */
-    public function testGetProducts()
+    public function testGetProducts(): void
     {
         $freepaid = new \Jacques\Freepaid\Client([
             'hostname' => 'ws.dev.freepaid.co.za',
@@ -256,18 +251,18 @@ class ClientTest extends \PHPUnit\Framework\TestCase
         ]);
         $response = $freepaid->getProducts();
 
-        $this->assertInstanceOf('\stdClass', $response);
+        $this->assertInstanceOf(\stdClass::class, $response);
         $this->assertEquals(1, $response->status);
         $this->assertEquals('000', $response->errorcode);
         $this->assertEquals('-', $response->message);
         $this->assertEquals('99981.90', $response->balance);
-        $this->assertInternalType('array', $response->products);
+        $this->assertIsArray($response->products);
         $this->assertCount(82, $response->products);
 
-        for ($i = 0; $i < 82; $i++) {
-            $this->assertInstanceOf('\stdClass', $response->products[$i]);
-            $this->assertInternalType('string', $response->products[$i]->description);
-            $this->assertInternalType('string', $response->products[$i]->network);
+        for ($i = 0; $i < 82; ++$i) {
+            $this->assertInstanceOf(\stdClass::class, $response->products[$i]);
+            $this->assertIsString($response->products[$i]->description);
+            $this->assertIsString($response->products[$i]->network);
 
             $this->assertNotNull($response->products[$i]->description);
             $this->assertNotNull($response->products[$i]->network);
@@ -276,7 +271,7 @@ class ClientTest extends \PHPUnit\Framework\TestCase
             $this->assertNotEmpty($response->products[$i]->description);
             $this->assertNotEmpty($response->products[$i]->network);
 
-            $this->assertRegexp('!\A(vodacom|pd?-vodacom|mtn|pd?-mtn|cellc|pd?-cellc|branson|telkom|neotel|heita|p-heita|worldcall|worldchat|eskom|bela)\z!', $response->products[$i]->network);
+            $this->assertMatchesRegularExpression('!\A(vodacom|pd?-vodacom|mtn|pd?-mtn|cellc|pd?-cellc|branson|telkom|neotel|heita|p-heita|worldcall|worldchat|eskom|bela)\z!', $response->products[$i]->network);
         }
 
         /*
